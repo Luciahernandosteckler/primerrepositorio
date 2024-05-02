6f8dcd09b593eae45b68e9868720b4372d58e995
luciahernandosteckler <luciahernandosteckler@gmail.com>
2024-05-02 18:54:59 -0300

    2

--- cursos/cfs/clase_2/ejercicio3.ts
+++ cursos/cfs/clase_2/ejercicio3.ts
@@ -5,8 +5,8 @@ const descuento = 10; // descuento en %
 const preciofinal = precio - (precio * descuento / 100);
 console.log("el precio a pagar es de: $" + preciofinal);
 
-//import * as readlinesync from ‘readline-sync’;    
+//import * as readlinesync from ‘readline-sync’;
 
-//let alturapersona = readlinesync.question(); 
+//let alturapersona = readlinesync.question();
 
 //console.log(alturapersona);
