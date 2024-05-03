b1916ee120a5ae41f8ada2d09e04538a2e1e507e
luciahernandosteckler <luciahernandosteckler@gmail.com>
2024-05-03 19:06:59 -0300

    tarea

--- cursos/cfs/clase_3/paroimpar.ts
+++ cursos/cfs/clase_3/paroimpar.ts
@@ -1,17 +1,33 @@
-import * as readlineSync from "readline-sync";
+import * as readlineSync from "readline-sync"; // Entrada de usuario 
 
-function determinarParImpar(numero: number): string {
-    if (numero === 0) {
-        return "El número es 0";
-    } else if (numero % 2 === 0) {
-        return "El número es par";
-    } else {
-        return "El número es impar";
-    }
+let numeroIngresado = readlineSync.question("Ingrese un número: ");
+let numero = +numeroIngresado; // Conversión de datos
+
+// Condiciones
+if (numero === 0) {
+    console.log("El número es 0");
+} else if (numero % 2 === 0) {
+    console.log("El número es par");
+} else {
+    console.log("El número es impar");
 }
 
-const numeroIngresado = readlineSync.question("Ingrese un número: ");
-const numero = parseInt(numeroIngresado);
+//Tambien se puede hacer declarando una función
 
-const resultado = determinarParImpar(numero);
-console.log(resultado);
+//import * as readlineSync from "readline-sync";
+
+//function determinarParImpar(numero: number): string {
+//    if (numero === 0) {
+//        return "El número es 0";
+//    } else if (numero % 2 === 0) {
+//        return "El número es par";
+//    } else {
+//        return "El número es impar";
+//    }
+//}
+//
+//const numeroIngresado = readlineSync.question("Ingrese un número: ");
+//const numero = parseInt(numeroIngresado);
+
+//const resultado = determinarParImpar(numero);
+//console.log(resultado);
